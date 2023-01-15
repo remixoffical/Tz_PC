8a39f617f62d4605d39ce5ad638513198a5ca897
Tz <85482530+remixoffical@users.noreply.github.com>
2023-01-14 23:25:09 -0800

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,7 +1,10 @@
 local gamelist = {
-    [994732206]  = "https://raw.githubusercontent.com/remixoffical/Tz_PC/main/BloxFruit.lua"
+   [994732206]  = "https://raw.githubusercontent.com/remixoffical/Tz_PC/main/BloxFruit.lua"
 }
 local checkgame = gamelist[game.gameId]
 if gamelist[game.gameId] then 
+print (checkgame)
     loadstring(game:HttpGet(checkgame))()
+else 
+game.Players.LocalPlayer:Kick("Not Game Support")
 end
