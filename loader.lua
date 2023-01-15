78bbada2188523a7c283fb20f56ddbed1b7da743
Tz <85482530+remixoffical@users.noreply.github.com>
2023-01-14 23:22:00 -0800

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,8 +1,6 @@
 local gamelist = {
     [994732206]  = "https://raw.githubusercontent.com/remixoffical/Tz_PC/main/BloxFruit.lua"
 }
-    [4520749081] = ""
-}
 local checkgame = gamelist[game.gameId]
 if gamelist[game.gameId] then 
     loadstring(game:HttpGet(checkgame))()
